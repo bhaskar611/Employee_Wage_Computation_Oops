1f7ad8f97780f4d4358a7e8c2a1a897476555870
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-03 23:48:19 +0530

    Added: Employee Wage For 100 hrs or 20 days

--- Employee_Wage_Computation_Oops/src/EmployeeWage.java
+++ Employee_Wage_Computation_Oops/src/EmployeeWage.java
@@ -5,14 +5,20 @@ public class EmployeeWage {
 	static final int IS_PART_TIME =2;
 	static final int EMP_RATE_PER_HRS=20;
 	static final int NUM_OF_WORKING_DAYS=20;
+	static final int MAX_HRS=100;
+
 	
 	
 	public static int caliculateEmployWage() {
 	int empHrs = 0;
 	 int empWage = 0;
 	 int totalEmpWage=0;
-	 for (int day=0; day < NUM_OF_WORKING_DAYS ; day++ ) {
-	 int empCheck =(int) Math.floor(Math.random()* 10) % 3;
+	 int totalEmpHrs=0;
+	 int count=0;
+	 while (totalEmpHrs <= MAX_HRS &&
+	         count < NUM_OF_WORKING_DAYS) {
+	     count++;
+     int empCheck =(int) Math.floor(Math.random()* 10) % 3;
 	 switch (empCheck){
 	    case IS_PART_TIME:
 	    empHrs=4;
@@ -23,11 +29,17 @@ public class EmployeeWage {
 	    default:
 	    empHrs=0;
 	    }
+	 totalEmpHrs += empHrs;
 	 
 //EMPLOYE WAGE
 	 empWage=empHrs * EMP_RATE_PER_HRS;
+	 System.out.println("Day#: "  + count + " Emp Hr: " +empHrs );
+	 
 	 System.out.println("  Emp Wage: " + empWage );
 	 totalEmpWage += empWage;
+// Total Employee Hours
+	 System.out.println(totalEmpHrs );
+	 
 	 }
 	
 //PRINT EMPLOYEWAGE
@@ -38,7 +50,10 @@ public class EmployeeWage {
 	
 			
 	}
+	
+	
 	public static void main(String[]args){
+//calling the Employe Wage
 		caliculateEmployWage();
 }
 }
